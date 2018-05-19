5ce1fc649e82835f4a618fc04983ce3bdcd0cfaf
Lonnell Ahiyya <lla@algometric.org>
2018-05-19 16:07:33 -0400

    Added flasher shell script, plus stand-alone tilde.

---
+++ flash.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+
+teensy_loader_cli -mmcu=atmega32u4 -w ergodox_ez_lahiyya.hex
--- keyboards/ergodox_ez/keymaps/lahiyya/keymap.c
+++ keyboards/ergodox_ez/keymaps/lahiyya/keymap.c
@@ -45,17 +45,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_APP,
         KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_ESC,
-        KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_SLSH,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,
         LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
-                                                     KC_GRV,  KC_LGUI,
+                                                     KC_TILD,  KC_LGUI,
                                                               KC_HOME,
                                                KC_CAPS,KC_LSFT,KC_END,
         // right hand
              KC_APP  ,    KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_BSPC ,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             KC_ENT  ,    KC_N,   KC_M,  KC_COMM,KC_DOT, KC_QUES,          KC_RSFT,
+             KC_ENT  ,    KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                   KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | EPRM  |      |      |      |      |                                       |      |    . |   0  |   =  |      |
- *   `-----------------------------------'                                       `----------------------------------'
+ 
  *                                        ,-------------.       ,-------------.
  *                                        |Animat|      |       |Toggle|Solid |
  *                                 ,------|------|------|       |------+------+------.
